7ddf6702dab4e46157ee7c8bd2ea80a96a89fb8c
denys <denys.akiyama@gmail.com>
2020-05-11 19:18:35 -0300

    cap-38: intoduzindo BeforeEach

--- src/test/java/guru/springframework/GreatingTest.java
+++ src/test/java/guru/springframework/GreatingTest.java
@@ -1,19 +1,26 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GreatingTest extends Greating {
 
+	private Greating greating;
+	
+	@BeforeEach
+	void setUpTesting () {
+		System.out.println("Antes de cada teste . . .");
+		greating = new Greating();
+	}
+	
 	@Test
 	void HelloWorld() {
-		Greating greating = new Greating();
 		
 		System.out.println(greating.helloWorld());
 	}
 
 	@Test
 	void helloWorld1() {
-		Greating greating = new Greating();
 		
 		System.out.println(greating.helloWorld("Denys"));
 	}
