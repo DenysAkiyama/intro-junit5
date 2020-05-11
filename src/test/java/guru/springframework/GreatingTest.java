4f543518d0fb5e6279208792fd35adb8e433dbe9
denys <denys.akiyama@gmail.com>
2020-05-11 19:29:37 -0300

    cap-39: JUnit 5 LifeCycle

--- src/test/java/guru/springframework/GreatingTest.java
+++ src/test/java/guru/springframework/GreatingTest.java
@@ -1,11 +1,19 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GreatingTest extends Greating {
 
 	private Greating greating;
+	
+	@BeforeAll
+	public static void beforeClass() {
+		System.out.println("Antes de tudo - iniciado apenas uma vez!!!");
+	}
 	
 	@BeforeEach
 	void setUpTesting () {
@@ -23,5 +31,24 @@ class GreatingTest extends Greating {
 	void helloWorld1() {
 		
 		System.out.println(greating.helloWorld("Denys"));
+	}
+	
+	@Test
+	void helloWorld2() {
+	
+		System.out.println(greating.helloWorld("Daniel"));
+		
+	}
+	
+	@AfterEach
+	void tearDown() {
+		
+		System.out.println("Depois de cada teste . . .");
+		
+	}
+	
+	@AfterAll
+	public static void afterClass() {
+		System.out.println("=========Depois de tudo - iniciado apenas uma vez!!!============");
 	}
 }
